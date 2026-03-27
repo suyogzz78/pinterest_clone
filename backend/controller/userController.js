15c7c96165f9e31ff379576c182679fabb1b4613
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-27 08:49:20 +0545

    feat(auth): implement user registration with bcrypt hashing and login endpoint

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,17 +1,37 @@
+const User = require("./../model/userModel");
+
+const bcrypt = require("bcrypt");
+
 async function registerUser(req, res) {
-    try{
+  try {
+    const { email, name, password } = req.body;
+    let user = await User.findOne({ email });
 
-    res.send('Register user');
+    if (user) {
+      return res.status(400).json({
+        message: "Account already exists",
+      });
     }
+    const hashpassword = await bcrypt.hash(password, 10);
 
-    catch(error){
-        console.error('Error registering user:', error);
-        res.status(500).json({ message: 'Server error' });
-    }
+     user = await User.create({
+      name,
+      password: hashpassword,
+      email
+    });
 
-
+    return res.status(201).json(
+        {
+            user,
+            message:"Account registered"
+        }
+    )
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 }
 
 module.exports = {
-    registerUser
-}
+  registerUser,
+};
