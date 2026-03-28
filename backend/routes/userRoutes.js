4da5d8095d7761c621cb1619c1c9f9ab9f627931
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-28 19:30:42 +0545

    feat(userroutes):created a new route for the profile page

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
-const { registerUser, loginUser } = require("../controller/userController");
+const { registerUser, loginUser, myProfile } = require("../controller/userController");
 const express = require("express");
+const {authmiddleware} = require('./../middleware/authmiddleware')
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me",authmiddleware,myProfile);
 
 module.exports = router;
