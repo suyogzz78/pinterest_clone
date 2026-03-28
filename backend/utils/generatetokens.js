d0271f953c0beed5e76924631e75ca78c92febed
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-28 19:31:28 +0545

    feat(utils):used jwt authentication with cookies:

--- backend/utils/generatetokens.js
+++ backend/utils/generatetokens.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 
 
 const generatettokens=(_id,res)=>{
-const tokens = jwt.sign({id},process.env.SECRET_KEY,{
+const tokens = jwt.sign({_id},process.env.SECRET_KEY,{
         expiresIn:"15d"
 })
 
-res.cookie("tokens",tokens,{
+res.cookie("token",tokens,{ 
     maxAge:15*24*60*60*1000,
     httpOnly:true,
     sameSite:"strict"
@@ -14,3 +14,5 @@ res.cookie("tokens",tokens,{
 
 
 };
+
+module.exports={generatettokens}
