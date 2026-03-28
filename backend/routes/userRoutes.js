7b8503d9e0fbc60f0d4d3f33aa6353d4fb2fcb98
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-28 23:18:00 +0545

    feat(userroutes): add user profile routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const { registerUser, loginUser, myProfile } = require("../controller/userController");
+const { registerUser, loginUser, myProfile,userProfile } = require("../controller/userController");
 const express = require("express");
 const {authmiddleware} = require('./../middleware/authmiddleware')
 const router = express.Router();
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/me",authmiddleware,myProfile);
+router.get("/:id",authmiddleware,userProfile);
 
 module.exports = router;
