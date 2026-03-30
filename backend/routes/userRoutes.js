a56321216e146443c1e95ddc8a51cc357ad159a7
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-30 19:27:23 +0545

    feat(userroutes):created a route for logout func

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
-const { registerUser, loginUser, myProfile,userProfile, followerandfollowing } = require("../controller/userController");
+const { registerUser, loginUser, myProfile,userProfile, followerandfollowing,logoutUser } = require("../controller/userController");
 const express = require("express");
 const {authmiddleware} = require('./../middleware/authmiddleware')
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout",authmiddleware,logoutUser);
 router.get("/me",authmiddleware,myProfile);
 router.get("/:id",authmiddleware,userProfile);
 router.post("/follow/:id",authmiddleware,followerandfollowing)
