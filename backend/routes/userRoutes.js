b5d089af7dd9132f07925bb144bcb6b58ff89156
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-30 17:18:30 +0545

    feat(routes): add follow/unfollow endpoint with auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const { registerUser, loginUser, myProfile,userProfile } = require("../controller/userController");
+const { registerUser, loginUser, myProfile,userProfile, followerandfollowing } = require("../controller/userController");
 const express = require("express");
 const {authmiddleware} = require('./../middleware/authmiddleware')
 const router = express.Router();
@@ -7,5 +7,6 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/me",authmiddleware,myProfile);
 router.get("/:id",authmiddleware,userProfile);
+router.post("/follow/:id",authmiddleware,followerandfollowing)
 
 module.exports = router;
