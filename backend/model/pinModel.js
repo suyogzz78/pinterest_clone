c4791b36920637f0676b1f594f8c54f2b2a411b0
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-31 17:50:57 +0545

    feat(pinmodel):added a new schema for the pin

---
+++ backend/model/pinModel.js
@@ -0,0 +1,46 @@
+const { urlencoded } = require("express");
+const mongoose = require("mongoose");
+
+const PinSchema = new mongoose.Schema({
+title:{
+    type:String,
+    required:true,
+},
+pin:{
+    type:String,
+    required:true,
+},
+owner :{
+    type: mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref:"User",
+},
+image:{
+    type:String,
+    url:String,
+},
+
+comments:[
+    {
+        user:{
+            type:String,
+            required:true,
+        },
+        name:{
+            type:String,
+            required:true,
+        },
+        comment:{
+            type:String,
+            required:true,
+        },
+        },
+]
+
+},{
+    timestamps:true,
+});
+
+const Pin = mongoose.model("Pin",PinSchema);
+
+module.exports = Pin;
