12b6ac1fe5db4945450b0d479ec91218687c530f
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-31 14:52:53 +0545

    added cloudinary package to store the images on cloud

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const connectDB = require('./database/db');
- const dotenv = require('dotenv');
- const userRoutes = require('./routes/userRoutes');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const connectDB = require("./database/db");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const cookieParser = require("cookie-parser");
+const cloudinary = require("cloudinary").v2;
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 
-
 const port = process.env.PORT || 3000;
-
 
 //middleware
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.use('/api/users',userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    connectDB();
+  console.log(`Server is running on port ${port}`);
+  connectDB();
 });
-
