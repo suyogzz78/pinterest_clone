62349ba208927520d8adfca8593072259691cf74
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-02 17:26:10 +0545

    feat:added route for fetching all the pins

--- backend/routes/pinRoutes.js
+++ backend/routes/pinRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { authmiddleware } = require("../middleware/authmiddleware");
 
 const { uploadfile } = require("../middleware/multer");
-const { createpin } = require("../controller/pinController");
+const { createpin ,getallpins,getpinbypinid} = require("../controller/pinController");
 
 const router = express.Router();
 
 router.post("/createpin",authmiddleware,uploadfile, createpin);
+router.get("/getpins",authmiddleware, getallpins);
+
 
 
 
