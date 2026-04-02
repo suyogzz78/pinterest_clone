b9178ae5a693423cffdc4833e3bbbdbc951aebce
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-02 16:38:09 +0545

    feat(pinRoutes):setup protected create pin route with image upload support

--- backend/routes/pinRoutes.js
+++ backend/routes/pinRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { authmiddleware } = require("../middleware/authmiddleware");
+
+const { uploadfile } = require("../middleware/multer");
+const { createpin } = require("../controller/pinController");
 
 const router = express.Router();
+
+router.post("/createpin",authmiddleware,uploadfile, createpin);
 
 
 
