796d1db54a3f1d646515a7bf8409bc25a6a81140
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-02 18:07:23 +0545

    added a controller for adding comments on pins

--- backend/routes/pinRoutes.js
+++ backend/routes/pinRoutes.js
@@ -2,15 +2,18 @@ const express = require("express");
 const { authmiddleware } = require("../middleware/authmiddleware");
 
 const { uploadfile } = require("../middleware/multer");
-const { createpin ,getallpins,getpinbypinid} = require("../controller/pinController");
+const {
+  createpin,
+  getallpins,
+  getpinbypinid,
+  commentonpins,
+} = require("../controller/pinController");
 
 const router = express.Router();
 
-router.post("/createpin",authmiddleware,uploadfile, createpin);
-router.get("/getpins",authmiddleware, getallpins);
-router.get("/getpin/:id",authmiddleware, getpinbypinid);
-
-
-
+router.post("/createpin", authmiddleware, uploadfile, createpin);
+router.get("/getpins", authmiddleware, getallpins);
+router.get("/getpin/:id", authmiddleware, getpinbypinid);
+router.post("/comment/:id", authmiddleware, commentonpins);
 
 module.exports = router;
