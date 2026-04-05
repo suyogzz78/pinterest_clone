95367db85e348bd9c2fa6e37abef749bfbfd7cfb
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-05 17:43:04 +0545

    feat:used react router dom for routing and imported the pages:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 
 import React from 'react'
 
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import Homepage from '../pages/Homepage'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+
 function App() {
   return (
-    <div className='bg-red-100 '>App</div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+
+
   )
 }
 
