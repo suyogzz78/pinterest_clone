507d49cab9c6612744a482cfd7158d6aac629c58
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-07 23:07:28 +0545

    feat: add reusable Loading spinner component with Tailwind animation

---
+++ frontend/components/Loading.jsx
@@ -0,0 +1,6 @@
+export const Loading = () => {
+    return(
+        <div className="inline-block h-5 w-5 border-2 border-t-2 border-r-transparent border-white rounded-full animate-spin "></div>
+
+    );
+}
