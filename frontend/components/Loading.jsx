903ad6b40ec6e4a672638d55fbb2cdffa9e09644
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-09 22:56:16 +0545

    added a new loading animation till the users data is fetched from backend

--- frontend/components/Loading.jsx
+++ frontend/components/Loading.jsx
@@ -4,3 +4,13 @@ export const Loading = () => {
 
     );
 }
+
+export const Loading2 = () => {
+    return(
+
+        <div className="flex items-center justify-center">
+            <div className="inline-block h-5 w-5 border-2 border-t-2 border-r-transparent border-white rounded-full animate-spin"></div>
+        </div>
+
+    )
+}
