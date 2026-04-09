43aa4d58ae0bf2af796804a130152cbff405e7b3
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-09 17:28:28 +0545

    feat(context);added registration logic in global context and passed the value

--- frontend/context/UserContext.jsx
+++ frontend/context/UserContext.jsx
@@ -10,6 +10,29 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState([]);
   const [btnloading, setbtnLoading] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function RegisterUser(name, email, password, navigate) {
+    // Implementation for registration logic
+
+    setbtnLoading(true);
+    try {
+      const { data } = await axios.post("/api/users/register", {
+        name,
+        email,
+        password,
+      }); // Assuming the response contains user data and a token
+      setUser(data.user); // Store user data in state
+      setIsAuthenticated(true); // Set authentication status to true
+      setbtnLoading(false); // Set loading to false after successful registration
+      toast.success(data.message); // Show success toast
+      navigate("/"); // Redirect to homepage after successful login
+    } catch (error) {
+      toast.error(
+        "Registration failed. Please check your credentials and try again.",
+      );
+      setbtnLoading(false); // Set loading to false after failed registration
+    }
+  }
   async function LoginUser(email, password, navigate) {
     // Implementation for login logic
 
@@ -52,6 +75,7 @@ export const UserProvider = ({ children }) => {
     <UserContext.Provider
       value={{
         LoginUser,
+        RegisterUser,
         user,
         btnloading,
         isAuthenticated,
