ac46dec20553acbd3b3d92e570cb12ae8899b52a
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-09 23:20:27 +0545

    added diffent links for the navbar menu also fetched the users name from the context to display the first letter of user

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
+import download from "../src/assets/download.png";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+
+const Navbar = ({user}) => {
   return (
-    <div>
-        Navbar
-      
-    </div>
-  )
-}
+    <div className="bg-white shadow-xl">
+      <div className=" mx-auto py-4 px-3 flex items-center justify-between gap-2">
+        <Link to="/" className="flex items-center gap-2">
+          <img src={download} alt="Download App" className="h-8 w-16" />
 
-export default Navbar
+          <span className="text-xl font-bold text-red-500 ">Pinterest</span>
+        </Link>
+
+        <div className="flex items-center gap-4">
+
+        <Link to="/" className="text-gray-500 hover:text-red-500">
+          Home
+        </Link>
+        <Link to="/create" className="text-gray-500 hover:text-red-500">
+          Create
+        </Link>
+
+        <Link to="/account" className="w-8 h-8 rounded-full bg-gray-500 text-white flex items-center justify-center hover:bg-red-500">
+          {user.name.slice(0, 1).toUpperCase()}
+        </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
