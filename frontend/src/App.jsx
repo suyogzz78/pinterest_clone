97e7bd31c8ff23b3c5eb7aef161f41534ced1297
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-09 14:38:11 +0545

    feat: add loading state to App for persistent login check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,22 @@ import Homepage from "../pages/Homepage";
 import Login from "../pages/login";
 import Register from "../pages/Register";
 import { useUser } from "../context/UserContext";
+import { Loading2 } from "../components/Loading";
+
+
+
 
 function App() {
-  const { user } = useUser(); // Now this will work
-  console.log(user); // should log "suyog"
+  const { user, isAuthenticated, loading } = useUser();
 
-  return (
+  return loading ? (
+    <Loading2 />
+  ) : (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={isAuthenticated ? <Homepage /> : <Login />} />
+        <Route path="/login" element={isAuthenticated ? <Homepage /> : <Login />} />
+        <Route path="/register" element={isAuthenticated ? <Homepage /> : <Register />} />
       </Routes>
     </BrowserRouter>
   );
