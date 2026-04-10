b1976b829b4958d2bb70753bc7c60918c3dd03f4
Suyog Gautam <sakurachhetri97@gmail.com>
2026-04-10 17:39:54 +0545

    feat: implement loading UI for pins fetch in Homepage

--- frontend/pages/Homepage.jsx
+++ frontend/pages/Homepage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { usePins } from '../context/PinContext'
 
 const Homepage = () => {
+  const { pins, loading } = usePins();
   return (
-    <div className='text-cyan-300'>
-      homepage
+    <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+      <div className='px-4 py-6 sm:px-0'>
+        <div className='flex rounded-lg border-4 border-dashed border-gray-200 flex-wrap'>
+          {pins.map((pin)=> (
+            <p key={pin._id}>{pin.title}</p>
+          ))}
+
+
+        </div>
+      </div>
+
     </div>
   )
+  
 }
 
 export default Homepage
